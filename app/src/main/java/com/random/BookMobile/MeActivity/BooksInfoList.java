f51a131a674e1a98ddd6ca7d68f7dfb61e05df46
Wayne <huwanyu94@gmail.com>
2019-03-14 12:49:14 +0800

    list of book in profile page done

--- app/src/main/java/com/random/BookMobile/MeActivity/BooksInfoList.java
+++ app/src/main/java/com/random/BookMobile/MeActivity/BooksInfoList.java
@@ -1,4 +1,71 @@
 package com.random.BookMobile.MeActivity;
 
-public class BooksInfoList {
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.random.BookMobile.LoginActivity;
+import com.random.BookMobile.MainActivity;
+import com.random.BookMobile.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class BooksInfoList extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+    private ArrayList<String> EventName = new ArrayList<>();
+    private List<Integer> EventID = new ArrayList<Integer>();
+    @Override
+    public void onBackPressed() {
+        Intent goBack = new Intent(BooksInfoList.this, MainActivity.class);
+        goBack.putExtra("id", 2);
+        startActivity(goBack);
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_of_books);
+
+//uncomment after database is up
+/*
+        DatabaseHelp dbHelper = new DatabaseHelp(this);
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+        Cursor eventCursor = dbHelper.viewMyEvent(LoginActivity.getUserID());
+
+
+        while (eventCursor.moveToNext())
+        {
+            int eventIndex = eventCursor.getColumnIndex("Event_Name");
+            int statusIndex = eventCursor.getColumnIndex("Event_Approval_Status");
+            String eventName = eventCursor.getString(eventIndex)+" ("+eventCursor.getString(statusIndex)+")";
+            EventName.add(eventName);
+
+            int IDIndex = eventCursor.getColumnIndex("Event_ID");
+            int eventID = eventCursor.getInt(IDIndex);
+            EventID.add(eventID);
+        }
+        db.close();
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.MeBookListRV);
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager((mLayoutManager));
+
+        mAdapter = new BooksInfoListAdapter(getBaseContext(), EventName, EventID);
+        mRecyclerView.setAdapter(mAdapter);
+
+
+*/
+
+    }
+
 }
--- app/src/main/java/com/random/BookMobile/MeActivity/BooksInfoListAdapter.java
+++ app/src/main/java/com/random/BookMobile/MeActivity/BooksInfoListAdapter.java
@@ -20,38 +20,37 @@ public class BooksInfoListAdapter extends RecyclerView.Adapter<BooksInfoListAdap
 
     private static Context context;
     private int totalCount;
-    private ArrayList<String> EventName;
-    private List<Integer> EventID;
+    private ArrayList<String> BookName;
+    private List<Integer> BookID;
 
-    public BooksInfoListAdapter(Context context, ArrayList<String> EventName, List<Integer> EventID) {
+    public BooksInfoListAdapter(Context context,ArrayList<String> BookName, List<Integer> BookID) {
         this.context = context;
         // get the number of EventName when the class is constructed
-        this.totalCount = EventName.size();
-        this.EventName = EventName;
-        this.EventID = EventID;
+        this.totalCount = BookName.size();
+        this.BookName = BookName;
+        this.BookID = BookID;
     }
-
-
     public static class ViewHolder extends RecyclerView.ViewHolder {
         // each data item is just a string in this case
         public Button button;
         public ViewHolder(View v) {
             super(v);
-            button = v.findViewById(R.id.bookButton);
+            button = v.findViewById(R.id.booklistButton);
         }
     }
+
 
     @NonNull
     @Override
     public BooksInfoListAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_list_of_books_adapter, parent, false);
-        ViewHolder vh = new ViewHolder(v);
+        BooksInfoListAdapter.ViewHolder vh = new BooksInfoListAdapter.ViewHolder(v);
         return vh;
     }
 
     @Override
     public void onBindViewHolder(@NonNull BooksInfoListAdapter.ViewHolder holder, final int position) {
-        holder.button.setText(EventName.get(position));
+        holder.button.setText(BookName.get(position));
 
         holder.button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -60,7 +59,7 @@ public class BooksInfoListAdapter extends RecyclerView.Adapter<BooksInfoListAdap
                 Intent startNewActivity = new Intent(context, DetailPage.class);
 
                 startNewActivity.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                startNewActivity.putExtra("BookID", Integer.toString(EventID.get(position)));
+                startNewActivity.putExtra("EventID", Integer.toString(BookID.get(position)));
 
                 context.startActivity(startNewActivity);
 
