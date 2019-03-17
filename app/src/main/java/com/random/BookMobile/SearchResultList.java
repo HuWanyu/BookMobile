32c7405a82174235fbaed51d573b792e9d6febab
Wayne <huwanyu94@gmail.com>
2019-03-17 14:13:03 +0800

    search page

--- app/src/main/java/com/random/BookMobile/SearchResultList.java
+++ app/src/main/java/com/random/BookMobile/SearchResultList.java
@@ -9,11 +9,16 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatDialogFragment;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.ProgressBar;
+import android.widget.SearchView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -30,13 +35,55 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import dmax.dialog.SpotsDialog;
 
 public class SearchResultList extends AppCompatDialogFragment {
 
-    
+    ListView searchbooklist;
+    ArrayAdapter<String> adapter;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_of_search_result);
+
+        searchbooklist = (ListView) findViewById(R.id.searchbooklist);
+
+        ArrayList<String> arrayBooks = new ArrayList<>();
+        arrayBooks.addAll(Arrays.asList(getResources().getStringArray(R.array.example_Books)));
+
+        adapter = new ArrayAdapter<String>(
+                SearchResultList.this
+                android.R.layout.simple_list_item_1,
+                arrayBooks
+        );
+
+        searchbooklist.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.list_of_search_result_menu, menu);
+        MenuItem item = menu.findItem(R.id.searchbooklist);
+        SearchView searchView = (SearchView) item.getActionView();
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                adapter.getFilter().filter(newText);
+                return false;
+            }
+        });
+        return true;
+    }
 }
 
 
